d8621f3d2f7c148ede1a6c7963c4f9764a7abab9
Koki Nanao <minsokosoko0107@gmail.com>
2023-09-15 10:16:08 +0900

    feat:追加
    コンポーネントの追加とエラーフックの作成

---
+++ src/features/Auth/hooks/useError.ts
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import { useNavigate } from 'react-router-dom'
+import { CsrfToken } from '../types'
+import useStore from '../store'
+
+export const useError = () => {
+  const navigate = useNavigate()
+}
